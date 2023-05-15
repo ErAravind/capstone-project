c288d064a2daa13eecd355edb3bdc2c3e1aa3be2
ErAravind <122307831+ErAravind@users.noreply.github.com>
2023-05-15 15:55:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
-docker compose up -d
+#!/bin/bash
+
+
+IMAGE_NAME=react-project
+IMAGE_VERSION=v1
+
+####
+docker tag $IMAGE_NAME:$IMAGE_VERSION aravindthiyagarajan/prod:$IMAGE_VERSION
+docker tag $IMAGE_NAME:$IMAGE_VERSION aravindthiyagarajan/dev:$IMAGE_VERSION
+
+docker push aravindthiyagarajan/dev:v1
+docker login --username aravindthiyagarajan --password devopsengineer@2023
+docker push aravindthiyagarajan/prod:v1
+docker logout
+~
+
